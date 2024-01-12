a51e7050919dd0178529e6c261ef7fa1dfe3d944
NK <nikke.kettunen07@gmail.com>
2024-01-12 11:57:48 +0200

    Harjoitus3?

---
+++ OlioJaWPFSovellukset/Harjoitus3 (KT)/Kiuas.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus3__KT_
+{
+    internal class Kiuas
+    {
+        private bool Päällä { get; set; }
+        private int Lämpötila {  get; set; }
+        private int Kosteus {  get; set; }
+        public Kiuas(bool _Päällä, int _Lämpötila, int _Kosteus)
+        {
+            Päällä = _Päällä;
+            Lämpötila = _Lämpötila;
+            Kosteus = _Kosteus;
+        }
+
+        public void Laitapäälle()
+        {
+            Päällä = true;
+        }
+        public void Laitapois()
+        {
+            Päällä = false;
+        }
+        public void Lämpötila_Kosteus(int _Lämpötila, int _Kosteus)
+        {
+            Lämpötila = _Lämpötila;
+            Kosteus = _Kosteus;
+        }
+        public void Tulosta()
+        {
+            Console.WriteLine($"Päällä {Päällä}, Lämpötila {Lämpötila}, Kosteus {Kosteus}");
+        }
+    }
+}
