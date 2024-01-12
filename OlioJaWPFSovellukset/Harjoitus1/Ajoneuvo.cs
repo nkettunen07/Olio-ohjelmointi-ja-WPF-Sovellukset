b5278215517bb5affde38d315e4b0f6e2162876a
NK <nikke.kettunen07@gmail.com>
2024-01-12 10:39:04 +0200

    Harjoitus1

---
+++ OlioJaWPFSovellukset/Harjoitus1/Ajoneuvo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus1
+{
+    internal class Ajoneuvo
+    {
+        public string Nimi { get; set; }
+        public int Nopeus { get; set; }
+        public int Renkaat { get; set; }
+        public Ajoneuvo(string _Nimi, int _Nopeus, int _Renkaat)
+        {
+            Nimi = _Nimi;
+            Nopeus = _Nopeus;
+            Renkaat = _Renkaat;
+        }
+
+        public void TulostaData()
+        {
+            // Mun pää hajos :3
+            Console.WriteLine(ToString());
+        }
+        public string ToString()
+        {
+            return Nimi + " " + Nopeus + "km/h " + Renkaat + " rengasta";
+        }
+    }
+}
--- OlioJaWPFSovellukset/Harjoitus1/Program.cs
+++ OlioJaWPFSovellukset/Harjoitus1/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Harjoitus1;
+
 Console.WriteLine("Hello, World! Testi!");
+
+
+Ajoneuvo Auto = new Ajoneuvo("Niauto", 10, 6);
+Auto.TulostaData();
