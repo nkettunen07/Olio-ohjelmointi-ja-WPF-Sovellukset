5f51a6377b91cb84dd1df4de58b9a8f18422c2eb
NK <nikke.kettunen07@gmail.com>
2024-01-15 09:41:12 +0200

    Harjoitus 4 vihaan tätä :)

---
+++ OlioJaWPFSovellukset/Harjoitus4/Albumi.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus4
+{
+    internal class Albumi
+    {
+        private string Artisti {  get; set; }
+        private string Nimi { get; set; }
+        private string Genre { get; set; }
+        private string Hinta { get; set; }
+        private List<Kappale> Kappaleet { get; set; }
+
+        public Albumi(string _Artisti, string _Nimi, string _Genre, string _Hinta, List<Kappale> _Kappaleet)
+        {
+            Artisti = _Artisti;
+            Nimi = _Nimi;
+            Genre = _Genre;
+            Hinta = _Hinta;
+            Kappaleet = _Kappaleet;
+        }
+
+        public void LisääKappale(string _Nimi, string _Pituus)
+        {
+            Kappaleet.Add(new Kappale(_Nimi, _Pituus));
+        }
+        public void Tulosta()
+        {
+            Console.WriteLine("Albumi:");
+            Console.WriteLine(" -Artisti: " + Artisti);
+            Console.WriteLine(" -Nimi: " + Nimi);
+            Console.WriteLine(" -Genre: " + Genre);
+            Console.WriteLine(" -Hinta: " + Hinta);
+            Console.WriteLine("Songs:");
+            foreach (Kappale i in Kappaleet)
+            {
+                Console.WriteLine(i.ToString());
+            }
+        }
+
+    }
+}
---
+++ OlioJaWPFSovellukset/Harjoitus4/Kappale.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus4
+{
+    internal class Kappale
+    {
+        private string Nimi {  get; set; }
+        private string Pituus { get; set; }
+        public Kappale(string _Nimi, string _Pituus)
+        {
+            Nimi = _Nimi;
+            Pituus = _Pituus;
+        }
+        public string ToString()
+        {
+            return $"--- Nimi: {Nimi} - ${Pituus}";
+        }
+    }
+}
