6ae4eed6d00cea319d6c1ef99c29d48fdf641367
NK <nikke.kettunen07@gmail.com>
2024-01-24 09:48:55 +0200

    harjoitus 5

---
+++ OlioJaWPFSovellukset/Harjoitus 5/Laskin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus_5
+{
+    public class Laskin
+    {
+        public static float Summa(float a, float b)
+        {
+            return a + b;
+        }
+        public static float Erotus(float a, float b)
+        {
+            return a - b;
+        }
+        public static float Kertolasku(float a, float b)
+        {
+            return a * b;
+        }
+        public static float Jako(float a, float b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ OlioJaWPFSovellukset/Harjoitus 5/Program.cs
@@ -0,0 +1,10 @@
+﻿using Harjoitus_5;
+
+float Summa = Laskin.Summa(1, 2);
+float Erotus= Laskin.Erotus(1, 2);
+float Kerto = Laskin.Kertolasku(1, 2);
+float Jako = Laskin.Jako(1, 2);
+Console.WriteLine($"Summa: {Summa}");
+Console.WriteLine($"Erotus: {Erotus}");
+Console.WriteLine($"Kerto: {Kerto}");
+Console.WriteLine($"Jako: {Jako}");
