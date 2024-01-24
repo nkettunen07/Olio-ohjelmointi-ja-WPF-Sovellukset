b7ef1139feb278cd34b4295e16f2f7cbfd1846ea
NK <nikke.kettunen07@gmail.com>
2024-01-24 10:12:21 +0200

    Harjoitus 6....!!!!!!!!!!!!

---
+++ OlioJaWPFSovellukset/Harjoitus6/Eläin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus6
+{
+    public class Eläin
+    {
+        public static int Olemassa = 0;
+        public virtual string? Tyyppi {  get; set; }
+        public virtual void OlemassaOlevat()
+        {
+            Console.WriteLine($"Olemassa olevia eläimiä on {Olemassa}");
+        }
+
+    }
+    public class Koira : Eläin
+    {
+        //public override string? Tyyppi { get; set; } // joo oon tyhmä.
+        public Koira()
+        {
+            Console.WriteLine("Tehtiin koira");
+            Olemassa++;
+            Tyyppi = "Koira";
+        }
+    }
+    public class Kissa : Eläin
+    {
+        public Kissa()
+        {
+            Console.WriteLine("Tehtiin kissa");
+            Olemassa++;
+            Tyyppi = "Kissa";
+        }
+    }
+
+}
---
+++ OlioJaWPFSovellukset/Harjoitus6/Program.cs
@@ -0,0 +1,8 @@
+﻿using Harjoitus6;
+
+Koira munkoira = new();
+Console.WriteLine($"Eläimiä ny pt 1: {Eläin.Olemassa}");
+Kissa munkissa = new();
+Console.WriteLine($"Eläimiä ny pt 2: {Eläin.Olemassa}");
+
+// WAOW....!!!!
