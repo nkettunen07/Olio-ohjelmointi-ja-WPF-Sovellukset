92e2453a1e4f83b9ee940f78952283ef5790605d
NK <nikke.kettunen07@gmail.com>
2024-01-25 11:04:15 +0200

    mitä tääki on 😭

---
+++ OlioJaWPFSovellukset/Harjoitus8/Henkilö.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus8
+{
+    public abstract class Henkilö
+    {
+        public string? Nimi;
+        public string? Työpaikka;
+        public int Palkka; // kk
+
+
+        public static int Pomoja = 0;
+    }
+    public class Työntekijä : Henkilö
+    {
+        public int ViikottaisetTunnit;
+        public Työntekijä(string nimi, string työpaikka, int palkka, int _vt)
+        {
+            Nimi = nimi;
+            Työpaikka = työpaikka;
+            Palkka = palkka;
+            ViikottaisetTunnit = _vt;
+        }
+
+    }
+    public class Pomo : Henkilö
+    {
+        public int bonus;
+        public string auto;
+        public Pomo(string nimi, string työpaikka, int palkka, int _bonus, string _auto)
+        {
+            if(Pomoja >= 1)
+            {
+                throw new Exception("Pomo on jo.");
+            }
+            Nimi = nimi;
+            Työpaikka = työpaikka;
+            Palkka = palkka;
+            bonus = _bonus;
+            auto = _auto;
+            Pomoja += 1;
+        }
+    }
+}
---
+++ OlioJaWPFSovellukset/Harjoitus8/Program.cs
@@ -0,0 +1,30 @@
+﻿using Harjoitus8;
+
+Henkilö Työntekijä1 = new Työntekijä("N", "C", 10, 6);
+Console.WriteLine(Työntekijä1);
+Henkilö Työntekijä2 = new Työntekijä("H", "C", 10, 6);
+Console.WriteLine(Työntekijä2);
+Henkilö Työntekijä3 = new Työntekijä("G", "C", 10, 6);
+Console.WriteLine(Työntekijä3);
+Henkilö Työntekijä4 = new Työntekijä("F", "C", 10, 6);
+Console.WriteLine(Työntekijä4);
+Henkilö Työntekijä5 = new Työntekijä("E", "C", 10, 6);
+Console.WriteLine(Työntekijä5);
+Henkilö Työntekijä6 = new Työntekijä("D", "C", 10, 6);
+Console.WriteLine(Työntekijä6);
+Henkilö Työntekijä7 = new Työntekijä("C", "C", 10, 6);
+Console.WriteLine(Työntekijä7);
+Henkilö Työntekijä8 = new Työntekijä("A", "C", 10, 6);
+Console.WriteLine(Työntekijä8);
+Henkilö Pomo = new Pomo("B", "C", 20, 300, "yutong");
+Console.WriteLine(Pomo);
+try
+{
+    Console.WriteLine("Tehdään toinen pomo.");
+    Henkilö Pomo2 = new Pomo("P", "C", 10, 200, "volov");
+    Console.WriteLine(Pomo2);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
