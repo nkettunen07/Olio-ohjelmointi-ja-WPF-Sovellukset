96d519bd9fc0c11a783d0a289b32ff4da7c7fd45
NK <nikke.kettunen07@gmail.com>
2024-01-25 10:46:20 +0200

    Harjoitus 7. Varmaan väärin mut katotaa

---
+++ OlioJaWPFSovellukset/Harjoitus7/Hissi.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus7
+{
+    internal class Hissi
+    {
+        private static int _kerros;
+        private int Kerros
+        {
+            get 
+            { 
+                return _kerros;
+            }
+            set
+            {
+                if(value >= 1 && value <= 6)
+                {
+                    _kerros = value;
+                    Console.WriteLine($"Hissi on nyt kerroksessa {_kerros}");
+                } else
+                {
+                    Console.WriteLine($"Antamasi kerros ei ole valitettavasti toimiva kerros. Anna kerros väliltä 1-6");
+                }
+            }
+        }
+        public void Asetakerros(int _krs)
+        {
+            Kerros = _krs;
+        }
+        public int kerrokset() { return _kerros; }
+    }
+}
---
+++ OlioJaWPFSovellukset/Harjoitus7/Program.cs
@@ -0,0 +1,10 @@
+﻿using Harjoitus7;
+
+Hissi elevaattori = new();
+while (true)
+{
+    Console.WriteLine($"Hissin kerros on {elevaattori.kerrokset()}. Anna kerros mihin haluat hissin.");
+    int outer = 0;
+    int.TryParse(Console.ReadLine(), out outer);
+    elevaattori.Asetakerros(outer);
+}
