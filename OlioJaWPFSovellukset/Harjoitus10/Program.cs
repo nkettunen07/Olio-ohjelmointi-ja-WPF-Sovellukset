f6fd868be84d4a3e8b764f7e6b9d542ca0f5f5c9
NK <nikke.kettunen07@gmail.com>
2024-03-25 12:13:08 +0200

    gitIGNORE

---
+++ OlioJaWPFSovellukset/Harjoitus10/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Harjoitus10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
