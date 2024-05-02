966f7e668ec9e1b71d8fc6cdaa2e92af4cee3c31
NK <nikke.kettunen07@gmail.com>
2024-05-02 10:28:54 +0300

    Harjoitus 23

---
+++ OlioJaWPFSovellukset/Harjoitus 23/Kiuas.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus_23
+{
+    internal class Kiuas
+    {
+        private bool Päällä { get; set; }
+        private int Lämpötila {  get; set;
+        }
+        private int Kosteus {  get; set; }
+        public Kiuas(bool _Päällä, int _Lämpötila, int _Kosteus)
+        {
+            Päällä = _Päällä;
+            if(_Lämpötila >= 0 && _Lämpötila <= 120) Lämpötila = _Lämpötila;
+            if (_Kosteus>= 0 && _Kosteus<= 100) Kosteus = _Kosteus;
+        }
+
+        public void Laitapäälle()
+        {
+            Päällä = true;
+        }
+        public void Laitapois()
+        {
+            Päällä = false;
+        }
+        public void Lämpötila_Kosteus(int _Lämpötila, int _Kosteus)
+        {
+            if (_Lämpötila >= 0 && _Lämpötila <= 120) Lämpötila = _Lämpötila;
+            if (_Kosteus >= 0 && _Kosteus <= 100) Kosteus = _Kosteus;
+        }
+        public void Tulosta()
+        {
+            Console.WriteLine($"Päällä {Päällä}, Lämpötila {Lämpötila}, Kosteus {Kosteus}");
+        }
+        public int getLämpötila()
+        {
+            return Lämpötila;
+        }
+        public int getKosteus()
+        {
+            return Kosteus;
+        }
+    }
+}
--- OlioJaWPFSovellukset/Harjoitus 23/MainWindow.xaml.cs
+++ OlioJaWPFSovellukset/Harjoitus 23/MainWindow.xaml.cs
@@ -20,9 +20,84 @@ namespace Harjoitus_23
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Kiuas kiuas = new Kiuas(true, 120, 100); // 120 lämpötila ja 100 kosteus defaultiksi nyt alkuun.
         public MainWindow()
         {
             InitializeComponent();
+            if(rdb_lämpötila != null) // checkkauksia vain, jos on olemassa niin checked =true
+            {
+                rdb_lämpötila.IsChecked = true;
+            } else if(rdb_kosteus != null)
+            {
+                rdb_kosteus.IsChecked = true;
+            }
+            RenderKiuas(null, null); // ja renderöidään null null.
+            // null null koska muuten se valittaa jos ei anna mitään ja jos tyhjentää argumentit nii muu osa sovelluksesta alkaa valittaa
+        }
+        private void RenderKiuas(object sender, RoutedEventArgs e)
+        {
+
+            // otetaan kiukaasta arvot
+            Lämpötila.Text = "Lämpötila: "+kiuas.getLämpötila().ToString();
+            Kosteus.Text = "Kosteus: "+kiuas.getKosteus().ToString();
+        }
+        private List<int> Numerot = new List<int>(); // sisältää numerot
+        private void RadioButtonManager(object sender, RoutedEventArgs e)
+        {
+            // tässä katsomme kumpi oli se jota clickattiin. jos oli lämpötila niin asetetaan kosteus falseen ja vice versa
+            RadioButton rdb = (RadioButton)sender;
+            if(rdb.Content.ToString().ToLower() == "lämpötila")
+            {
+                rdb_kosteus.IsChecked = false;
+                rdb_lämpötila.IsChecked = true;
+            } else
+            {
+                rdb_kosteus.IsChecked = true;
+                rdb_lämpötila.IsChecked = false;
+            }
+            // käytin tässä hyvin epäeettisiä tapoja tehdä tämän mutta tulee helpottumaan kun lisää lisää kuraa sovellukseen.
+        }
+
+        private void NumPadManager(object sender, RoutedEventArgs e)
+        {
+            // katsomme mikä content buttonissa oli ja parsetamme sen ja lisäämme numerolistaan
+            Button npd = (Button)sender;
+            int numbertoadd = int.Parse(npd.Content.ToString().Replace(" ", ""));
+            Numerot.Add(numbertoadd);
+            RenderTextBox(sender, e); // ja lopuksi renderöidään tekstiboxi
+        }
+        private void RenderTextBox(object sender, RoutedEventArgs e)
+        {
+            numerot.Text = String.Join("", Numerot.ToArray()); // joinataan numerot listassa stringiin esim 1, 2, 3 => 123
+        }
+
+        private void NumPadPoista(object sender, RoutedEventArgs e)
+        {
+            // tein virheen käyttämällä listiä mut tääkin toimii lol
+            Numerot.RemoveAt(Numerot.Count - 1);
+        }
+
+        private void NumPadEnter(object sender, RoutedEventArgs e)
+        {
+            int UusiMuoto;
+            int.TryParse(
+                String.Join(
+                    "",
+                    Numerot.ToArray()
+                    ).ToString(), // teemme listasta string muotoisen ja sitten parsemme se
+                out UusiMuoto
+                ); // parsetamme numerot UusiMuoto muuttujaat
+            int UusiLämpötila = rdb_lämpötila.IsChecked == true
+                ? UusiMuoto
+                : kiuas.getLämpötila();
+            // Tarkistamme molemmissa kumpi on valittu ja jos oli valittu niin käytämme uuttamuotoa ja jos ei ollut niin fallback aiempaan numeroon
+            int UusiKosteus = rdb_kosteus.IsChecked == true
+                ? UusiMuoto
+                : kiuas.getKosteus();
+            kiuas.Lämpötila_Kosteus(UusiLämpötila, UusiKosteus); // asetamme kiukaan arvot
+            RenderKiuas(sender, e); //renderöidään kiukaan arvot
+            Numerot.Clear(); // tyhjennämme numerolistan
+            RenderTextBox(sender, e); // renderöidään numeromanageri
         }
     }
 }
